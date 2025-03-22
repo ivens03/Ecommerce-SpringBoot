d2d918283ced16c589dfe17ef5251cdc6ef87fe7
ivens magno <ivensmagno@gmail.com>
2025-03-22 11:48:30 -0300

    ligacao com o banco de dados

---
+++ src/main/java/Ecommerce/servocosProdutos/ServocosProdutosApplication.java
@@ -0,0 +1,13 @@
+package Ecommerce.servocosProdutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServocosProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServocosProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Ecommerce/servocosProdutos/ServocosProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package Ecommerce.servocosProdutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServocosProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
